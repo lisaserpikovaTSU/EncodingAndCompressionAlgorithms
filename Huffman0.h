9641863426e5021114090b4ecc038eb345155ce3
liza <liza.serpikova@stud.tsu.ru>
2024-02-05 20:46:51 +0700

    объявлен класс Node для списка частот

--- Huffman0.h
+++ Huffman0.h
@@ -2,9 +2,24 @@
 #ifndef ENCODINGANDCOMPRESSIONALGORITHMS_HUFFMAN0_H
 #define ENCODINGANDCOMPRESSIONALGORITHMS_HUFFMAN0_H
 
+#include <iostream>
+#include <fstream>
+#include <string>
 
-class Huffman0 {
-
+//список частот с пом класса ноде
+class Node{
+private:
+    std::string str;
+    int freq = 0;
+    Node *left = nullptr;
+    Node *right = nullptr;
+public:
+    Node(std::string s, int fr=0, Node *L=NULL, Node *R=NULL){
+        str = s;
+        freq = fr;
+        left = L;
+        right = R;
+    }
 };
 
 
